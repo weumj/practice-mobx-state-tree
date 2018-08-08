2cd45bdd5f27d4803c5f692fe13c4eb8f06e921d
Myungjin Kim <weumj1@gmail.com>
2018-08-08 15:42:55 +0900

    feat: actions

--- src/model/WishList.test.ts
+++ src/model/WishList.test.ts
@@ -8,6 +8,9 @@ it("can create a instance of a model", () => {
 
   expect(item.price).toBe(28.73);
   expect(item.image).toBe("");
+
+  item.changeName("Name");
+  expect(item.name).toBe("Name");
 });
 
 it("can create a wishlist", () => {
@@ -22,4 +25,21 @@ it("can create a wishlist", () => {
 
   expect(list.items.length).toBe(1);
   expect(list.items[0].price).toBe(28.73);
+});
+
+it("can add new items", () => {
+  const list = WishList.create();
+
+  list.add(
+    WishListItem.create({
+      name: "Chesterton",
+      price: 10,
+    }),
+  );
+
+  expect(list.items).toHaveLength(1);
+  expect(list.items[0].name).toBe("Chesterton");
+
+  list.items[0].changeName("Book of G.K. Chesterton");
+  expect(list.items[0].name).toBe("Book of G.K. Chesterton");
 });
--- src/model/WishList.ts
+++ src/model/WishList.ts
@@ -1,18 +1,36 @@
 import { types } from "mobx-state-tree";
 
-export const WishListItem = types.model({
-  name: types.string,
-  price: types.number,
-  image: "",
-});
-
-export const WishList = types.model({
-  items: types.optional(types.array(WishListItem), []),
-});
+export const WishListItem = types
+  .model({
+    name: types.string,
+    price: types.number,
+    image: "",
+  })
+  .actions(self => ({
+    changeName(newName: string) {
+      self.name = newName;
+    },
+    changePrice(newPrice: number) {
+      self.price = newPrice;
+    },
+    changeImage(newImage: string) {
+      self.image = newImage;
+    },
+  }));
 
 type WishListItemType = typeof WishListItem.Type;
-type WishListType = typeof WishList.Type;
-
 export interface IWishListItem extends WishListItemType {}
+
+export const WishList = types
+  .model({
+    items: types.optional(types.array(WishListItem), []),
+  })
+  .actions(self => ({
+    add(item: IWishListItem) {
+      self.items.push(item);
+    },
+  }));
+
+type WishListType = typeof WishList.Type;
 
 export interface IWishList extends WishListType {}
