64862681f06ef50a653217aa2b26550fa79f7273
Myungjin Kim <weumj1@gmail.com>
2018-08-09 18:35:32 +0900

    feat: persistance

--- src/index.tsx
+++ src/index.tsx
@@ -5,26 +5,45 @@ import App from "./App";
 import "./assets/index.css";
 
 import { WishList } from "./models/WishList";
+import { onSnapshot } from "mobx-state-tree";
 
-const wishList = WishList.create({
-  items: [
-    {
-      name: "LEGO Mindstorms EV3",
-      price: 349.95,
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg",
-    },
-    {
-      name: "Miracles - C.S. Lewis",
-      price: 12.91,
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51a7xaMpneL._SX329_BO1,204,203,200_.jpg",
-    },
-  ],
+const _STORAGE_ID_ = "wishlistapp";
+
+let initState;
+
+if (localStorage.getItem(_STORAGE_ID_)) {
+  const json = JSON.parse(localStorage.getItem(_STORAGE_ID_) as string);
+
+  if (WishList.is(json)) {
+    initState = json;
+  }
+} else {
+  initState = {
+    items: [
+      {
+        name: "LEGO Mindstorms EV3",
+        price: 349.95,
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg",
+      },
+      {
+        name: "Miracles - C.S. Lewis",
+        price: 12.91,
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/51a7xaMpneL._SX329_BO1,204,203,200_.jpg",
+      },
+    ],
+  };
+}
+
+const wishList = WishList.create(initState);
+
+onSnapshot(wishList, snapshot => {
+  localStorage.setItem(_STORAGE_ID_, JSON.stringify(snapshot));
 });
 
 ReactDOM.render(<App wishList={wishList} />, document.getElementById("app"));
 
-setInterval(() => {
-  wishList.items[0].changePrice(wishList.items[0].price + 1);
-}, 2000);
+// setInterval(() => {
+//   wishList.items[0].changePrice(wishList.items[0].price + 1);
+// }, 2000);
